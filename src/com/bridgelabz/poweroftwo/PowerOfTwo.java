da79ad9ef9e4b57df1afd36df42e284908b8aec9
Rohit <rohitkamble10792@gmail.com>
2022-05-22 15:46:24 +0530

    checked Leap year or not

--- src/com/bridgelabz/poweroftwo/PowerOfTwo.java
+++ src/com/bridgelabz/poweroftwo/PowerOfTwo.java
@@ -1,7 +1,32 @@
 package com.bridgelabz.poweroftwo;
-
+import com.bridgelabz.util.Utility;
 public class PowerOfTwo {
     public static void main(String[] args) {
-        System.out.println("Welcome to PowerofTwo Program");
+
+
+        // INITIALIZATION
+        int num, mul = 1;
+
+        // DATA INPUT THROUGH COMMAND LINE
+        num = Integer.parseInt(args[0]);
+
+        // CONDITION CHECK
+        if (num > 31) {
+            System.out.println("value of number should be less than 31");
+            return;
+        }
+
+        // COMPUTATION
+        else {
+            System.out.println(mul);
+            for (int j = 1; j <= num; j++) {
+                mul = mul * 2;
+                System.out.println(mul);
+            }
+        }
+
     }
+
 }
+
+
